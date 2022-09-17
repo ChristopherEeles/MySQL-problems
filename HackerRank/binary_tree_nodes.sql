10a2656185491109b3de6bc30debd29c6fbb6888
Christopher Eeles <christopher.eeles@outlook.com>
2022-09-17 18:12:02 -0400

    Add solotuion for Binary Tree structure

--- HackerRank/binary_tree_nodes.sql
+++ HackerRank/binary_tree_nodes.sql
@@ -27,4 +27,34 @@ VALUES
 
 SELECT * FROM BST;
 
+-- Question
+
+/*
+You are given a table, BST, containing two columns: N and P, where N
+represents the value of a node in Binary Tree, and P is the parent of N.
+
+Write a query to find the node type of Binary Tree ordered by the value of
+the node. Output one of the following for each node:
+    Root: If node is root node.
+    Leaf: If node is leaf node.
+    Inner: If node is neither root nor leaf node.
+*/
+
 -- Solution
+
+/*
+Root node has no parent
+Inner node has and is parent
+Leaf nodes is not parent
+*/
+
+SELECT
+    N,
+    CASE
+        WHEN P IS NULL THEN 'Root'
+        WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+        ELSE 'Leaf'
+    END
+FROM
+    BST
+ORDER BY N;
